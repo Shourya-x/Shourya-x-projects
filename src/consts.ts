cc6c6fe5ec20f2a4288790d2d43effa78642e022
Shourya-x <spacefoxxy@SpaceFoxxy-Win.>
2025-02-23 08:33:16 +0000

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -13,13 +13,13 @@ export type Link = {
 }
 
 export const SITE: Site = {
-  TITLE: 'astro-erudite',
+  TITLE: 'Shourya\'s Project',
   DESCRIPTION:
-    'astro-erudite is a opinionated, no-frills blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  EMAIL: 'jason@enscribe.dev',
+    'Hi, I\'m Shourya, an aspiring electronics engineer from India 🇮🇳, passionate about space and robotics. And this is my Projects page',
+  EMAIL: 'pshourya83@gmail.com',
   NUM_POSTS_ON_HOMEPAGE: 2,
   POSTS_PER_PAGE: 3,
-  SITEURL: 'https://astro-erudite.vercel.app',
+  SITEURL: 'https://shourya.engineer',
 }
 
 export const NAV_LINKS: Link[] = [
@@ -30,8 +30,8 @@ export const NAV_LINKS: Link[] = [
 ]
 
 export const SOCIAL_LINKS: Link[] = [
-  { href: 'https://github.com/jktrn', label: 'GitHub' },
-  { href: 'https://twitter.com/enscry', label: 'Twitter' },
-  { href: 'jason@enscribe.dev', label: 'Email' },
-  { href: '/rss.xml', label: 'RSS' },
+  { href: 'https://github.com/Shourya-x', label: 'GitHub' },
+  { href: 'https://shourya.engineer', label: 'Website' },
+  { href: 'pshourya83@gmail.com', label: 'Email' },
+  { href: 'https://www.linkedin.com/in/shourya-pandey-14952b235/', label: 'LinkedIn' },
 ]
---
+++ src/content/authors/Shourya.md
@@ -0,0 +1,10 @@
+---
+name: "Shourya"
+pronouns: "he/him"
+avatar: "https://shourya.engineer/me.png"
+bio: "Hi, I'm Shourya Pandey"
+website: "https://shourya.engineer"
+github: "https://github.com/Shourya-x"
+linkedin: "https://www.linkedin.com/in/shourya-pandey-14952b235/"  # Changed to lowercase
+mail: "pshourya83@gmail.com"
+---
